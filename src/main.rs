058ae2c334af652bcdf55b595e85b61c9097aa9f
Alexander Rowland <rowlanda@protonmail.com>
2021-02-21 23:15:45 -0800

    Add variable shadowing example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Variables are immutable by default, so you must use the mut keyword in order to make them mutable.
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // Variables can be shadowed by using the same name and the let keyword.
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
 }
